8105e33b1fcce78815cd00de9228242de286c74d
veyner <myfun239@mail.ru>
2018-07-13 22:55:11 +0300

    init

---
+++ Magic/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Magic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Фамилия = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(172, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 106);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Сохранить данные";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Фамилия
+            // 
+            this.Фамилия.AutoSize = true;
+            this.Фамилия.Location = new System.Drawing.Point(12, 6);
+            this.Фамилия.Name = "Фамилия";
+            this.Фамилия.Size = new System.Drawing.Size(56, 13);
+            this.Фамилия.TabIndex = 2;
+            this.Фамилия.Text = "Фамилия";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(172, 106);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(142, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Загрузить данные";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Фамилия);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label Фамилия;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Magic/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace Magic
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var student = new Student();
+            student.Surname = textBox1.Text;
+            if (!Directory.Exists(Properties.Settings.Default.PathToData))
+            {
+                Directory.CreateDirectory(Properties.Settings.Default.PathToData);
+            }
+            SaveData(student);
+            MessageBox.Show($"Данные {student.Surname} сохранены");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        public void SaveData(Student student)
+        {
+            var fullSavePath = Path.Combine(Properties.Settings.Default.PathToData, student.Surname + ".json");
+
+            using (var writer = new StreamWriter(fullSavePath))
+            {
+                var json = JsonConvert.SerializeObject(student);
+                writer.Write(json);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var student = LoadSave("Hui");
+            textBox1.Text = student.Surname;
+        }
+
+        public Student LoadSave(string studensSurname)
+        {
+            var fullDataPath = Path.Combine(Properties.Settings.Default.PathToData, studensSurname + ".json");
+            using (var reader = new StreamReader(fullDataPath))
+            {
+                var json = reader.ReadToEnd();
+                return JsonConvert.DeserializeObject<Student>(json);
+            }
+        }
+    }
+}
